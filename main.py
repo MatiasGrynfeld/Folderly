d8d90481a31cbcfdec84d9cb4629b83987b9c0cb
MatiasGrynfeld <48519558@est.ort.edu.ar>
2024-12-08 18:06:31 -0300

    started app with tkinter

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
-from DirItems.FolderManager import FolderManager
+import tkinter as tk
+from tkinter import ttk
+# Window config
+app = tk.Tk()
+app.resizable(False, False)
+app.title("Folderly")
+window_width = 1000
+window_height = 800
 
-banned = set(['.venv', '__pycache__', '.git'])
-folder_manager = FolderManager(r"C:\Users\48519558\Desktop", banned)
-folder_manager.print()
+screen_width = app.winfo_screenwidth()
+screen_height = app.winfo_screenheight()
+
+position_top = int(screen_height / 2 - window_height / 2)
+position_right = int(screen_width / 2 - window_width / 2)
+
+app.geometry(f"{window_width}x{window_height}+{position_right}+{position_top}")
+app.iconbitmap('./Assets/icon.ico')
+
+# Frames
+
+titleFrame = ttk.Frame(app, width=window_width)
+appFrame = ttk.Frame(app, width=window_width, height=window_height-titleFrame.winfo_height())
+
+# Widgets
+
+title = ttk.Label(titleFrame, text="Folderly", font=("Arial", 20), background='red', anchor='center', justify='center')
+label = ttk.Label(appFrame, text="Hello World", font=("Arial", 20), background='blue', anchor='center', justify='center')
+
+# Object placement
+
+titleFrame.pack(side="top", fill="both")
+appFrame.pack(side="top", expand=True, fill="both")
+title.pack(side="top", fill="both")
+label.pack(side="top", expand=True, fill="both")
+# Start App
+app.mainloop()
